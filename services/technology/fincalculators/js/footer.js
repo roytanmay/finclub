4f6a8dcb61ba13178fe51a6da15a8d6b08e472b9
Tanmay Roy <tanmayroy84368@gmail.com>
2023-08-18 19:14:05 +0530

    initial commit

---
+++ services/technology/fincalculators/js/footer.js
@@ -0,0 +1,88 @@
+document.getElementById("header").innerHTML = `<footer id="footer">
+  <div class="footer-top">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-3 col-md-6 footer-contact">
+          <h3>FinClub JU</h3>
+          <p>
+            <span style="color:white;"><i class="bi bi-envelope-at-fill"></i> &nbsp;finclubju@gmail.com</span><br><br>
+            <strong>Jadavpur University - Jadavpur Campus</strong><br>
+            188, Raja Subodh Chandra Mallick Road<br>
+            Jadavpur, Kolkata, WB 700032<br><br>
+            <strong>Jadavpur University - Salt Lake Campus</strong><br>
+            B-73-80, Plot No. 8, Salt Lake Bypass, LB Block<br>
+            Sector 3, Bidhannagar, Kolkata, WB 700098<br>              
+          </p>
+        </div>
+        <div class="col-lg-2 col-md-6 footer-links">
+          <h4>Useful Links</h4>
+          <ul>
+            <li><i class="bx bx-chevron-right"></i> <a href="../../../../index.html">Home</a></li>
+            <li><i class="bx bx-chevron-right"></i> <a href="../../../../blog.html">Blog</a></li>
+            <li><i class="bx bx-chevron-right"></i> <a href="../../../../shop.html">Shop</a></li>
+            <li><i class="bx bx-chevron-right"></i> <a href="../../../../about/faqs.html">FAQs</a></li>
+            <li><i class="bx bx-chevron-right"></i> <a href="../../../../events.html">Events</a></li>
+            <li><i class="bx bx-chevron-right"></i> <a href="../../../../archives.html">Archives</a></li>
+            <li><i class="bx bx-chevron-right"></i> <a href="../../../../projects.html">Projects</a></li>
+            <li><i class="bx bx-chevron-right"></i> <a href="../../../../contact.html">Contact Info</a></li>
+            <li><i class="bx bx-chevron-right"></i> <a href="../../../../about/terms-of-service.html">Terms of Service</a></li>
+            <li><i class="bx bx-chevron-right"></i> <a href="../../../../about/privacy-policy.html">Privacy Policy</a></li>
+          </ul>
+        </div>
+        <div class="col-lg-2 col-md-6 footer-links">
+          <h4>Our Work and Services</h4>
+          <ul>
+            <li><i class="bx bx-chevron-right"></i> <a href="../../../../work/finance.html"><b>Finance</b></a></li>
+            <li><i class="bx bx-chevron-right"></i> <a href="../../../../work/investment.html"><b>Investment</b></a></li>
+            <li><i class="bx bx-chevron-right"></i> <a href="../../../../work/social-impact.html"><b>Social Impact</b></a></li>
+            <li><i class="bx bx-chevron-right"></i> <a href="../../../../work/consulting.html"><b>Consulting</b></a></li>
+            <li><i class="bx bx-chevron-right"></i> <a href="../../../../work/analytics.html"><b>Analytics</b></a></li>
+            <li><i class="bx bx-chevron-right"></i> <a href="../../../../work/literacy.html"><b>Literacy</b></a></li>
+            <li><i class="bx bx-chevron-right"></i> <a href="../../../../services/consultancy.html">Consultancy Services</a></li>
+            <li><i class="bx bx-chevron-right"></i> <a href="../../../../about/partners-and-collabs.html">Partners &amp; Collabs</a></li>
+            <li><i class="bx bx-chevron-right"></i> <a href="../../../../about/activity-reports.html">Activity Reports</a></li>
+            <li><i class="bx bx-chevron-right"></i> <a href="../../../../join.html">Membership</a></li>
+          </ul>
+        </div>
+        <div class="col-lg-2 col-md-6 footer-links">
+          <h4><br></h4>
+          <ul>
+            <li><i class="bx bx-chevron-right"></i> <a href="../../../../services/technology.html#finlabs-software">FinLabs Software</a></li>
+            <li><i class="bx bx-chevron-right"></i> <a href="../../../../services/technology.html#algo-backtesting">Algo Backtesting</a></li>
+            <li><i class="bx bx-chevron-right"></i> <a href="../../../../services/technology.html#market-data-feeds">Market Data Feeds</a></li>
+            <li><i class="bx bx-chevron-right"></i> <a href="../../../../services/technology.html#mock-stock-trading">Mock Stock Trading</a></li>
+            <li><i class="bx bx-chevron-right"></i> <a href="../../../../services/technology.html#financial-calculators">Financial Calculators</a></li>
+            <li><i class="bx bx-chevron-right"></i> <a href="../../../../services/training.html#certificate-courses">Certificate Courses</a></li>
+            <li><i class="bx bx-chevron-right"></i> <a href="../../../../services/training.html#classroom-sessions">Classroom Sessions</a></li>
+            <li><i class="bx bx-chevron-right"></i> <a href="../../../../services/training.html#quizzes-and-contests">Quizzes &amp; Contests</a></li>
+            <li><i class="bx bx-chevron-right"></i> <a href="../../../../services/training.html#guest-seminars">Guest Seminars</a></li>
+            <li><i class="bx bx-chevron-right"></i> <a href="../../../../services/training.html#workshops">Workshops</a></li>
+          </ul>
+        </div>
+        <div class="col-lg-3 col-md-6 footer-newsletter">
+          <h4>Join Our Newsletter</h4>
+          <p>Subscribe to our newsletter to get exclusive access to our latest articles.</p>
+          <a href="../../../../subscribe.html">
+            <button style="font-size: 16px; padding: 0 20px; background: #f03c02; color: #fff; transition: 0.3s; border-radius: 4px 4px 4px 4px; height:40px;" onMouseOver="this.style.background='#fd5c28'" onMouseOut="this.style.background='#f03c02'">Subscribe</button>
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="container d-md-flex py-4">
+    <div class="me-md-auto text-center text-md-start">
+      <div class="copyright">
+        &copy; Copyright <strong><span>Finance Club, Jadavpur University</span></strong>. All Rights Reserved.
+      </div>
+      <div class="credits">
+        Designed with ❤️ by <a href="../../../../developer.html">developers</a> at FinClub JU.
+      </div>
+    </div>
+    <div class="social-links text-center text-md-right pt-3 pt-md-0">
+      <a href="https://twitter.com/finclubju" target="_blank" class="twitter"><i class="bx bxl-twitter"></i></a>
+      <a href="https://facebook.com/finclubju" target="_blank" class="facebook"><i class="bx bxl-facebook"></i></a>
+      <a href="https://instagram.com/finclubju" target="_blank" class="instagram"><i class="bx bxl-instagram"></i></a>
+      <a href="https://linkedin.com/company/finclubju" target="_blank" class="linkedin"><i class="bx bxl-linkedin"></i></i></a>
+    </div>
+  </div>
+</footer>`;
---
+++ services/technology/fincalculators/js/header.js
@@ -0,0 +1,93 @@
+document.getElementById(
+  "header"
+).innerHTML = `<section id="topbar" class="d-flex align-items-center" style="height:32px;">
+<div class="container d-flex justify-content-center justify-content-md-between">
+  <div class="contact-info d-flex align-items-center">
+    <i class="bi bi-award-fill d-flex align-items-center" style="color:#00cf4f;"><a href="../../../../courses.html"><b>Courses</b></a></i>
+    <i class="bi bi-trophy-fill d-flex align-items-center ms-4" style="color:#f5ca0c;"><a href="../../../../contests.html"><b>Contests</b></a></i>
+    <i class="bi bi-currency-exchange d-flex align-items-center ms-4" style="color:#ff6a6a;"><a href="../../../../finlabs.html"><b>FinLabs</b></a></i>
+  </div>
+  <div class="social-links d-none d-md-flex align-items-center">
+    <a href="https://twitter.com/finclubju" target="_blank" class="twitter"><i class="bi bi-twitter"></i></a>
+    <a href="https://facebook.com/finclubju" target="_blank" class="facebook"><i class="bi bi-facebook"></i></a>
+    <a href="https://instagram.com/finclubju" target="_blank" class="instagram"><i class="bi bi-instagram"></i></a>
+    <a href="https://linkedin.com/company/finclubju" target="_blank" class="linkedin"><i class="bi bi-linkedin"></i></i></a>
+  </div>
+</div>
+</section>
+<!-- Header -->
+<header id="header" class="d-flex align-items-center">
+<div class="container d-flex justify-content-between">
+  <div class="logo">
+    <h1 class="text-light"><a href="../../../../index.html"><b>FinClub</b> JU</a></h1>
+  </div>
+  <nav id="navbar" class="navbar">
+    <ul>
+      <li><a class="active" href="../../../../index.html"><b>Home</b></a></li>
+      <li><a href="../../../../blog.html"><b>Blog</b></a></li>
+      <li><a href="../../../../shop.html"><b>Shop</b></a></li>
+      <li class="dropdown"><a><span><b>Our Work</b></span> <i class="bi bi-chevron-down"></i></a>
+        <ul>
+          <li><a href="../../../../work/finance.html">Finance<i class="bi bi-bank2"></i></a></li>
+          <li><a href="../../../../work/investment.html">Investment<i class="bi bi-cash-coin"></i></a></li>
+          <li><a href="../../../../work/social-impact.html">Social Impact<i class="bi bi-buildings-fill"></i></a></li>
+          <li><a href="../../../../work/consulting.html">Consulting<i class="bi bi-journal-bookmark-fill"></i></a></li>
+          <li><a href="../../../../work/analytics.html">Analytics<i class="bi bi-bar-chart-fill"></i></a></li>
+          <li><a href="../../../../work/literacy.html">Literacy<i class="bi bi-book-half"></i></a></li>
+        </ul>
+      </li>
+      <li class="dropdown"><a><span><b>Our Services</b></span> <i class="bi bi-chevron-down"></i></a>
+        <ul>
+          <li class="dropdown"><a><span>Consultancy</span> <i class="bi bi-chevron-right"></i></a>
+            <ul>
+              <li><a href="../../../../services/consultancy.html#social-consulting">Social</a></li>
+              <li><a href="../../../../services/consultancy.html#corporate-consulting">Corporate</a></li>
+              <li><a href="../../../../services/consultancy.html#live-projects">Live Projects</a></li>
+            </ul>
+          </li>
+          <li class="dropdown"><a><span>Technology</span> <i class="bi bi-chevron-right"></i></a>
+            <ul>
+              <li><a href="../../../../services/technology.html#finlabs-software">FinLabs Software</a></li>
+              <li><a href="../../../../services/technology.html#algo-backtesting">Algo Backtesting</a></li>                  
+              <li><a href="../../../../services/technology.html#market-data-feeds">Market Data Feeds</a></li>
+              <li><a href="../../../../services/technology.html#mock-stock-trading">Mock Stock Trading</a></li>
+              <li><a href="../../../../services/technology.html#financial-calculators">Financial Calculators</a></li>
+            </ul>
+          </li>
+          <li class="dropdown"><a><span>Training</span> <i class="bi bi-chevron-right"></i></a>
+            <ul>
+              <li><a href="../../../../services/training.html#certificate-courses">Certificate Courses</a></li>
+              <li><a href="../../../../services/training.html#classroom-sessions">Classroom Sessions</a></li>
+              <li><a href="../../../../services/training.html#quizzes-and-contests">Quizzes &amp; Contests</a></li>
+              <li><a href="../../../../services/training.html#guest-seminars">Guest Seminars</a></li>
+              <li><a href="../../../../services/training.html#workshops">Workshops</a></li>                
+            </ul>
+          </li>
+        </ul>
+      </li>
+      <li class="dropdown"><a><span><b>About Us</b></span> <i class="bi bi-chevron-down"></i></a>
+        <ul>
+          <li><a href="../../../../about/our-story.html">Our Story</a></li>
+          <li><a href="../../../../about/team-finclub.html">Team FinClub</a></li>
+          <li><a href="../../../../about/board-of-advisors.html">Board of Advisors</a></li>
+          <li><a href="../../../../about/partners-and-collabs.html">Partners &amp; Collabs</a></li>
+          <li><a href="../../../../about/community-groups.html">Community Groups</a></li>
+          <li><a href="../../../../about/activity-reports.html">Activity Reports</a></li>
+          <li><a href="../../../../join.html">Membership</a></li>
+          <li><a href="../../../../about/testimonials.html">Testimonials</a></li>
+          <li><a href="../../../../contact.html">Contact Info</a></li>
+          <li><a href="../../../../about/faqs.html">FAQs</a></li>
+        </ul>
+      </li>
+      <li class="dropdown"><a><span><b>More</b></span> <i class="bi bi-chevron-down"></i></a>
+        <ul>
+          <li><a href="../../../../events.html">Events</a></li>
+          <li><a href="../../../../archives.html">Archives</a></li>
+          <li><a href="../../../../projects.html">Projects</a></li>
+        </ul>
+      </li>
+    </ul>
+    <i class="bi bi-list mobile-nav-toggle"></i>
+  </nav>
+</div>
+</header>`;
